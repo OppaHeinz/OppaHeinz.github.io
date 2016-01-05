41ef87dbefbeae792777f78eedb3d67b0a4a666b
Christian Grefer <christian.grefer@etecture.de>
2016-01-05 16:55:04 +0100

    Major improvements

--- js/soundboard.js
+++ js/soundboard.js
@@ -1,16 +1,73 @@
 /* globals AudioContext, BufferLoader */
 window.onload = init;
+
 var context;
 var bufferLoader;
 
+var mapping =
+  [{
+    high: 161,
+    regular: 49,
+    low: 33,
+  }, {
+    high: 8220,
+    regular: 50,
+    low: 34,
+  }, {
+    high: 182,
+    regular: 51,
+    low: 167,
+  }, {
+    high: 162,
+    regular: 52,
+    low: 36,
+  }, {
+    high: 91,
+    regular: 53,
+    low: 37,
+  }, {
+    high: 93,
+    regular: 54,
+    low: 38,
+  }, {
+    high: 124,
+    regular: 55,
+    low: 47,
+  }, {
+    high: 123,
+    regular: 56,
+    low: 40,
+  }, {
+    high: 125,
+    regular: 57,
+    low: 41,
+  }, {
+    high: 8800,
+    regular: 48,
+    low: 61,
+  }, {
+    high: 171,
+    regular: 113,
+    low: 81,
+  }, {
+    high: 8721,
+    regular: 119,
+    low: 87,
+  }, {
+    high: 8364,
+    regular: 101,
+    low: 69,
+  },
+];
+
 function init() {
+
   // Fix up prefixing
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   context = new AudioContext();
   1;
   bufferLoader = new BufferLoader(
-    context,
-    [
+    context, [
       'sounds/alarm.mp3',
       'sounds/bloody.mp3',
       'sounds/haha-williams.mp3',
@@ -26,7 +83,7 @@ function init() {
       'sounds/im-not-a-fool.mp3',
     ],
     finishedLoading
-    );
+  );
 
   bufferLoader.load();
 }
@@ -34,126 +91,43 @@ function init() {
 function finishedLoading(bufferList) {
   // Create two sources and play them both together.
 
-  function play(index, slow) {
+  function start(index, speed) {
     var source = context.createBufferSource();
-    source.buffer = bufferList[index];
     source.connect(context.destination);
+    source.playbackRate.value = speed;
+    source.buffer = bufferList[index];
     source.start(0);
-    slow ? source.playbackRate.value = 0.5 : source.playbackRate.value = 1;
+  }
+
+  function play(keyCode) {
+    mapping.map(function(value, key) {
+      if (keyCode === value.high) {
+        start(key, 1.2);
+      }
+
+      if (keyCode === value.regular) {
+        start(key, 1);
+      }
+
+      if (keyCode === value.low) {
+        start(key, 0.8);
+      }
+    });
   }
 
   document.onkeypress = function(e) {
-
-    console.log(e.charCode);
-
-    switch (e.charCode){
-
-      case 49:
-        play(0);
-      break;
-
-      case 50:
-        play(1);
-      break;
-
-      case 51:
-        play(2);
-      break;
-
-      case 52:
-        play(3);
-      break;
-
-      case 53:
-        play(4);
-      break;
-
-      case 54:
-        play(5);
-      break;
-
-      case 55:
-        play(6);
-      break;
-
-      case 56:
-        play(7);
-      break;
-
-      case 57:
-        play(8);
-      break;
-
-      case 48:
-        play(9);
-      break;
-
-      case 113:
-        play(10);
-      break;
-
-      case 119:
-        play(11);
-      break;
-
-      case 101:
-        play(12);
-      break;
-
-//// SLOWED DOWN
-
-      case 33:
-        play(0, true);
-      break;
-
-      case 34:
-        play(1, true);
-      break;
-
-      case 167:
-        play(2, true);
-      break;
-
-      case 36:
-        play(3, true);
-      break;
-
-      case 37:
-        play(4, true);
-      break;
-
-      case 38:
-        play(5, true);
-      break;
-
-      case 47:
-        play(6, true);
-      break;
-
-      case 40:
-        play(7, true);
-      break;
-
-      case 41:
-        play(8, true);
-      break;
-
-      case 61:
-        play(9, true);
-      break;
-
-      case 81:
-        play(10, true);
-      break;
-
-      case 87:
-        play(11, true);
-      break;
-
-      case 69:
-        play(12, true);
-      break;
-    }
+    e.preventDefault();
+    console.log(e.keyCode);
+    play(e.keyCode);
   };
+
+  var clickTargets = document.getElementsByClassName('clickable');
+
+  for (var i = 0; i < clickTargets.length; i++) {
+    clickTargets[i].addEventListener('click', function() {
+      console.log(this.attributes['data-keycode'].value);
+      play(parseInt(this.attributes['data-keycode'].value));
+    });
+  }
 
 }
